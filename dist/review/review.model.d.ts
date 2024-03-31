5e8c2c3f9df0b0d753017c1eff6b3e5c7e1ad901
ElkaMaclaud <elkaMaclaud@gmail.com>
2024-03-31 16:12:02 +0300

    first commit

---
+++ dist/review/review.model.d.ts
@@ -0,0 +1,10 @@
+import { Base, TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+export interface ReviewModel extends Base {
+}
+export declare class ReviewModel extends TimeStamps {
+    name: string;
+    title: string;
+    description: string;
+    rating: number;
+    createdAt: Date;
+}
